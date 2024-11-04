af39fad0defbe540ada7cf2c3dffe47b55eca9a5
stmfusijhb024 <stmfusijhb024@student.wethinkcode.co.za>
2024-11-04 20:44:14 +0200

    added high scores and fixed osme names

--- snake.py
+++ snake.py
@@ -1,16 +1,15 @@
 import random
 import pygame
 import time
-
+import os
 pygame.init()
 
 # Colors
-colour_1 = (255, 255, 255)
-colour_2 = (255, 255, 102)
-colour_3 = (0, 0, 0)
-colour_4 = (213, 200, 80)
-colour_5 = (0, 255, 0)
-colour_6 = (255, 0, 0)
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+green = (0, 255, 0)
+red = (255, 0, 0)
 
 
 field_len = 800
@@ -19,17 +18,69 @@ caption = pygame.display.set_mode((field_len, field_height))
 pygame.display.set_caption('Snake')
 timer = pygame.time.Clock()
 snake_size = 10
-snake_speed = 10
+snake_speed = 15
 display_style = pygame.font.SysFont('impact', 24)
 score_font = pygame.font.SysFont('impact', 30)
+high_score_file = 'highscores.txt'
+def load_high_score():
+    scores = []
+    if os.path.exists(high_score_file):
+        with open(high_score_file, 'r') as file:
+            for line in file:
+                try:
+                    name, score = line.strip().split(',')
+                    scores.append((name.strip(), int(score)))  
+                except ValueError:
+                    print(f"Skipping line due to ValueError: {line.strip()}")
+    return sorted(scores, key=lambda x: x[1], reverse=True)[:10]
 
+def save_score(name,score):
+    scores = load_high_score()  
+    scores.append((name.strip(), score))  
+    scores = sorted(scores, key=lambda x: x[1], reverse=True)[:10]  
+    with open(high_score_file, 'w') as file:
+        for name, score in scores:  
+            file.write(f'{name},{score}\n')
+def show_high_scores():
+    caption.fill(white)
+    high_scores = load_high_score()
+    title = display_style.render('High scores', True, green)
+    caption.blit(title, [field_len / 3, field_height / 6])
+    for i, (name,score) in enumerate(high_scores, 1):
+        score_text = score_font.render(f'{i}. {name}: {score}', True, black)
+        caption.blit = (score_text, [field_len/3 , field_height/6 + i* 30])
+        pygame.display.update()
+        pygame.time.delay(7000)
+def get_name():
+    entering_name = True
+    name = ''
+    while entering_name:
+        caption.fill(white)
+        prompt = display_style.render("Enter your name and press Enter:", True, black)
+        name_text = display_style.render(name, True, black)
+        caption.blit(prompt, [field_len / 4, field_height / 3])
+        caption.blit(name_text, [field_len / 4, field_height / 3 + 50])
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:  # Enter key
+                    entering_name = False
+                elif event.key == pygame.K_BACKSPACE:  # Backspace key
+                    name = name[:-1]
+                else:
+                    name += event.unicode  # Add character to name
+    return name.strip() or "Anonymous"  # Use "Anonymous" if no name is entered
 def final_score(score):
-    val = score_font.render('SNAKE!!     Current Score: ' + str(score), True, colour_2)
+    val = score_font.render(f'SNAKE!!Current Score: {score}', True, yellow)
     caption.blit(val, [0, 0])
 
 def create_snake(snake_block, snake_list):
     for i in snake_list:
-        pygame.draw.rect(caption, colour_3, [i[0], i[1], snake_block, snake_block])
+        pygame.draw.rect(caption, black, [i[0], i[1], snake_block, snake_block])
 
 def display_message(msg, colour):
     message = display_style.render(msg, True, colour)
@@ -51,17 +102,21 @@ def game():
 
     while not play:
         while game_close:
-            caption.fill(colour_6)
-            display_message('You lose! Press C to play again or Q to quit', colour_3)
+            caption.fill(red)
+            display_message('You lose! Press R to play again or Q to quit', black)
             final_score(snake_len - 1)
             pygame.display.update()
 
             for i in pygame.event.get():
                 if i.type == pygame.KEYDOWN:
                     if i.key == pygame.K_q:
+                        player_name = get_name()
+                        save_score(player_name,snake_len - 1)
                         play = True
                         game_close = False
-                    if i.key == pygame.K_c:
+                    if i.key == pygame.K_r:
+                        player_name = get_name()
+                        save_score(player_name,snake_len - 1)
                         game()
         
         for event in pygame.event.get():
@@ -86,10 +141,10 @@ def game():
 
         val_x1 += new_x1
         val_y1 += new_y1
-        caption.fill(colour_4)
+        caption.fill(green)
 
         
-        pygame.draw.rect(caption, colour_5, [foodx_pos, foody_pos, snake_size, snake_size])
+        pygame.draw.rect(caption, red, [foodx_pos, foody_pos, snake_size, snake_size])
 
         
         snake_head = [val_x1, val_y1]
